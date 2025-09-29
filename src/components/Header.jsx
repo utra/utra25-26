4d785a500704b9b55e1b5a7262f143158c4fc488
amey-khatri <amey.khatri@gmail.com>
2025-09-29 12:39:41 -0400

    Made header fixed to top of page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,34 +26,41 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full px-4 bg-black text-white">
-      <div className="max-w-6xl mx-auto">
-        <nav className="flex items-center justify-between">
-          <Link to="/">
-            <img src={utraLogo} alt="UTRA Logo" className="max-w-[180px]" />
-          </Link>
-          <ul className="flex items-center space-x-10 py-[30px]">
-            {navItems.map((item) => (
-              <li key={item.name}>
-                {item.external ? (
-                  <a
-                    href={item.path}
-                    className="relative hover:text-purple-400 transition-colors duration-500 after:content-[''] after:absolute after:left-0 after:top-[calc(100%+10px)] after:h-0.5 after:w-0 after:bg-purple-400 after:transition-all after:duration-500 hover:after:w-full"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {item.name}
-                  </a>
-                ) : (
-                  <Link to={item.path} className={getNavItemClasses(item.path)}>
-                    {item.name}
-                  </Link>
-                )}
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <>
+      <header className="w-full px-4 bg-black text-white fixed top-0 z-50">
+        <div className="max-w-6xl mx-auto">
+          <nav className="flex items-center justify-between">
+            <Link to="/">
+              <img src={utraLogo} alt="UTRA Logo" className="max-w-[180px]" />
+            </Link>
+            <ul className="flex items-center space-x-10 py-[30px]">
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  {item.external ? (
+                    <a
+                      href={item.path}
+                      className="relative hover:text-purple-400 transition-colors duration-500 after:content-[''] after:absolute after:left-0 after:top-[calc(100%+10px)] after:h-0.5 after:w-0 after:bg-purple-400 after:transition-all after:duration-500 hover:after:w-full"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.name}
+                    </a>
+                  ) : (
+                    <Link
+                      to={item.path}
+                      className={getNavItemClasses(item.path)}
+                    >
+                      {item.name}
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {/* Spacer to prevent content from going under fixed header */}
+      <div className="h-[84px]"></div>
+    </>
   );
 }
