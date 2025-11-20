fe85ed71ce90b234b1a0f4db156d954480e510e2
amey-khatri <amey.khatri@gmail.com>
2025-11-20 02:14:40 -0500

    Added background combat image and gradient overlay

--- src/pages/teams/CombatPage.jsx
+++ src/pages/teams/CombatPage.jsx
@@ -1,12 +1,35 @@
+import bgGradient from "../../assets/images/graphics/background_gradient.png";
+import combatphoto from "../../assets/images/subteams/combat/annie.jpg";
+
 export default function CombatPage() {
   return (
-    <main className="relative min-h-screen bg-black">
-      <div className="mx-auto max-w-6xl px-4 pt-8">
-        <h1 className="text-3xl font-semibold text-white mb-4 text-center pt-8">
-          Combat Robotics
-        </h1>
-        <p className="text-gray-300 text-center">Content coming soon...</p>
+    <div className="min-h-screen">
+      <div className="absolute inset-0 max-h-[700px] bg-[linear-gradient(180deg,rgba(170,165,190,1)_0%,rgba(190,185,210,1)_20%,rgba(175,170,200,1)_34%,rgba(145,150,200,1)_98%)]">
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat mix-blend-multiply opacity-90"
+          style={{
+            backgroundImage: `url('${combatphoto}')`,
+            backgroundPosition: "center 20%",
+          }}
+        />
       </div>
-    </main>
+
+      <div className="">
+        <div
+          className="absolute top-[700px] left-0 right-0 bottom-0 bg-cover bg-center pointer-events-none"
+          style={{
+            backgroundImage: `url(${bgGradient})`,
+          }}
+        />
+        <div className="mx-auto max-w-6xl px-4 relative z-10">
+          <div className="relative top-[150px] h-[300px] w-[700px] bg-[rgba(60,60,60,0.52)] rounded-3xl flex items-center">
+            {/* Foreground "Combat" text in Afacad font - stacked on top */}
+            <p className="text-white [font-family:'Afacad',sans-serif] font-bold text-[100px] relative z-10 left-10 drop-shadow-[0_1px_2px_rgba(0,0,0,0.8)]">
+              Combat
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
