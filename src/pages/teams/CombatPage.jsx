cdc09bd76417f75d09d991cad1a5afac9f3513fb
amey-khatri <amey.khatri@gmail.com>
2025-11-20 14:49:57 -0500

    Fixed combat photo and gradient

--- src/pages/teams/CombatPage.jsx
+++ src/pages/teams/CombatPage.jsx
@@ -3,32 +3,34 @@ import combatphoto from "../../assets/images/subteams/combat/annie.jpg";
 
 export default function CombatPage() {
   return (
-    <div className="min-h-screen">
-      <div className="absolute inset-0 max-h-[700px] bg-[linear-gradient(180deg,rgba(170,165,190,1)_0%,rgba(190,185,210,1)_20%,rgba(175,170,200,1)_34%,rgba(145,150,200,1)_98%)]">
-        <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat mix-blend-multiply opacity-90"
-          style={{
-            backgroundImage: `url('${combatphoto}')`,
-            backgroundPosition: "center 20%",
-          }}
-        />
+    <div className="flex flex-col items-center bg-[linear-gradient(180deg,#4F4B60_0%,#6D6886_20%,#575078_34%,#36397E_98%)] min-h-[2000px] h-screen">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat mix-blend-multiply opacity-90 fade-bottom max-h-[600px]"
+        style={{
+          backgroundImage: `url('${combatphoto}')`,
+          backgroundPosition: "center 15%",
+        }}
+      />
+
+      <div className="relative flex flex-col pt-[150px] w-full px-[350px]">
+        <div className="justify-center h-[150px] w-[600px]">
+          <h1 className="text-white [font-family:'Afacad',sans-serif] font-bold text-[100px]">
+            Combat
+          </h1>
+          <p className="text-white [font-family:'ProximaNova',sans-serif] text-[24px]">
+            {" "}
+            High power robots built for competitive clashes.
+          </p>
+        </div>
       </div>
 
-      <div className="">
+      <div className="relative w-full h-[1800px] mt-[-20px] pt-[200px] flex flex-col items-center">
         <div
-          className="absolute top-[700px] left-0 right-0 bottom-0 bg-cover bg-center pointer-events-none"
+          className="absolute inset-0 bg-cover h-full fade-top pointer-events-none"
           style={{
             backgroundImage: `url(${bgGradient})`,
           }}
         />
-        <div className="mx-auto max-w-6xl px-4 relative z-10">
-          <div className="relative top-[150px] h-[300px] w-[700px] bg-[rgba(60,60,60,0.52)] rounded-3xl flex items-center">
-            {/* Foreground "Combat" text in Afacad font - stacked on top */}
-            <p className="text-white [font-family:'Afacad',sans-serif] font-bold text-[100px] relative z-10 left-10 drop-shadow-[0_1px_2px_rgba(0,0,0,0.8)]">
-              Combat
-            </p>
-          </div>
-        </div>
       </div>
     </div>
   );
