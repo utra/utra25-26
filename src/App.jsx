6ffef8019da2240254ce1dcf6c312c554fe0ce98
amey-khatri <amey.khatri@gmail.com>
2026-07-01 20:00:10 -0400

    added tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Router from "./components/Router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
+function Analytics() {
+  const location = useLocation();
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("event", "page_view", { page_path: location.pathname });
+    }
+  }, [location]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <div>
         <Header />
         <div className="flex-grow">
