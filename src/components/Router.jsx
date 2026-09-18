9d4521a280ea05f19eadb5526e1e93297d925147
amey-khatri <amey.khatri@gmail.com>
2026-09-18 11:19:17 -0400

    Added blank rift page to build later and instand redirect

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -24,16 +24,7 @@ import WebPage from "../pages/teams/WebPage";
 import MarketingPage from "../pages/teams/MarketingPage";
 import RobonarsPage from "../pages/teams/RobonarsPage";
 import OutreachPage from "../pages/teams/OutreachPage";
-
-const RIFT_APPLY_URL =
-  "https://utra.notion.site/3d747514ebfe80de97bde8e87b96631c?pvs=105";
-
-function ExternalRedirect({ to }) {
-  useEffect(() => {
-    window.location.replace(to);
-  }, [to]);
-  return null;
-}
+import RiftPage from "../pages/teams/RiftPage";
 
 // Scroll to top on route change (except back/forward navigation)
 function ScrollToTop() {
@@ -71,14 +62,7 @@ export default function Router() {
         <Route path="/teams/outreach" element={<OutreachPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/complaints" element={<ComplaintsPage />} />
-        <Route
-          path="/rift"
-          element={<ExternalRedirect to={RIFT_APPLY_URL} />}
-        />
-        <Route
-          path="/rift/apply"
-          element={<ExternalRedirect to={RIFT_APPLY_URL} />}
-        />
+        <Route path="/teams/rift" element={<RiftPage />} />
       </Routes>
     </>
   );
