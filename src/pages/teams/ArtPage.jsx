88ba875bb870604bd0aa1c09fd31b2b46749ba0b
amey-khatri <amey.khatri@gmail.com>
2026-09-22 12:45:47 -0400

    Updated art images with new pictures and removed unsued images

--- src/pages/teams/ArtPage.jsx
+++ src/pages/teams/ArtPage.jsx
@@ -46,16 +46,23 @@ const highlights = [
 
 const team_images = [
   {
-    name: "art1",
+    name: "art-team",
     img: new URL(
-      "../../assets/images/subteams/art/Art robot.jpg",
+      "../../assets/images/subteams/art/art-team.jpg",
       import.meta.url,
     ).href,
   },
   {
-    name: "art2",
+    name: "art-working",
     img: new URL(
-      "../../assets/images/subteams/art/20230601_161926.jpg",
+      "../../assets/images/subteams/art/art-working.jpg",
+      import.meta.url,
+    ).href,
+  },
+  {
+    name: "art-pavement",
+    img: new URL(
+      "../../assets/images/subteams/art/art-pavement.jpg",
       import.meta.url,
     ).href,
   },
@@ -70,20 +77,6 @@ const team_images = [
     name: "art4",
     img: new URL(
       "../../assets/images/subteams/art/20230604_204124.jpg",
-      import.meta.url,
-    ).href,
-  },
-  {
-    name: "art5",
-    img: new URL(
-      "../../assets/images/subteams/art/IMG_4167.jpg",
-      import.meta.url,
-    ).href,
-  },
-  {
-    name: "art7",
-    img: new URL(
-      "../../assets/images/subteams/art/IMG_5199.JPG",
       import.meta.url,
     ).href,
   },
